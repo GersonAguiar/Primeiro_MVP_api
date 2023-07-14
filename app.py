0aeb51faabdc73868293e211f2b12ed2c86e994b
GersonAguiar <38675620+GersonAguiar@users.noreply.github.com>
2023-07-14 14:13:38 -0300

    Primeira tentativa de API

---
+++ app.py
@@ -0,0 +1,171 @@
+from flask_openapi3 import OpenAPI, Info, Tag
+from flask import redirect
+from urllib.parse import unquote
+
+from sqlalchemy.exc import IntegrityError
+
+from model import Session, Cliente, Comentario
+from logger import logger
+from schemas import *
+from flask_cors import CORS
+
+info = Info(title="Minha API", version="1.0.0")
+app = OpenAPI(__name__, info=info)
+CORS(app)
+
+# definindo tags
+home_tag = Tag(name="Documentação", description="Seleção de documentação: Swagger, Redoc ou RapiDoc")
+cliente_tag = Tag(name="Cliente", description="Adição, visualização e remoção de clientes com relação à base")
+comentario_tag = Tag(name="Comentario", description="Adição de um comentário a clientes cadastrado na base")
+
+
+@app.get('/', tags=[home_tag])
+def home():
+    """Redireciona para /openapi, tela que permite a escolha do estilo de documentação.
+    """
+    return redirect('/openapi')
+
+
+@app.post('/cliente', tags=[cliente_tag],
+          responses={"200": ClienteViewSchema, "409": ErrorSchema, "400": ErrorSchema})
+def add_cliente(form: ClienteSchema):
+    """Adiciona um novo Cliente à base de dados
+
+    Retorna uma representação dos clientes e comentários associados.
+    """
+    cliente = Cliente(
+        nome=form.nome,
+        numeroCPF=form.numeroCPF,
+        valor=form.valor)
+    logger.debug(f"Adicionando cliente de nome: '{cliente.nome}'")
+    try:
+        # cria conexão com a base
+        session = Session()
+        # adiciona cliente
+        session.add(cliente)
+        # realiza o camando de adição de novo cliente na tabela
+        session.commit()
+        logger.debug(f"Adicionado cliente de nome: '{cliente.nome}'")
+        return apresenta_cliente(cliente), 200
+
+    except IntegrityError as e:
+        # duplicidade do nome é a provável razão do IntegrityError
+        # na presente listagem a duplicidade do nome não é um problema
+        error_msg = "Cliente de mesmo nome já salvo na base :/"
+        logger.warning(f"Erro ao adicionar cliente '{cliente.nome}', {error_msg}")
+        return {"mesage": error_msg}, 409
+
+    except Exception as e:
+        # caso um erro fora do previsto
+        error_msg = "Não foi possível salvar novo item :/"
+        logger.warning(f"Erro ao adicionar cliente '{cliente.nome}', {error_msg}")
+        return {"mesage": error_msg}, 400
+
+
+@app.get('/clientes', tags=[cliente_tag],
+         responses={"200": ListagemClientesSchema, "404": ErrorSchema})
+def get_clientes():
+    """Faz a busca por todos os Clientes cadastrados
+
+    Retorna uma representação da listagem de clientes.
+    """
+    logger.debug(f"Coletando clientes ")
+    # cria conexão com a base
+    session = Session()
+    # realiza a busca
+    clientes = session.query(Cliente).all()
+
+    if not clientes:
+        # se não há clientes cadastrados
+        return {"clientes": []}, 200
+    else:
+        logger.debug(f"%d clientes econtrados" % len(clientes))
+        # retorna a representação de cliente
+        print(clientes)
+        return apresenta_clientes(clientes), 200
+
+
+@app.get('/cliente', tags=[cliente_tag],
+         responses={"200": ClienteViewSchema, "404": ErrorSchema})
+def get_cliente(query: ClienteBuscaSchema):
+    """Faz a busca por um Cliente a partir do id do cliente
+
+    Retorna uma representação dos clientes.
+    """
+    cliente_id = query.id
+    logger.debug(f"Coletando dados sobre cliente #{cliente_id}")
+    # criando conexão com a base
+    session = Session()
+    # faz a busca
+    cliente = session.query(Cliente).filter(Cliente == cliente_id).first()
+
+    if not cliente:
+        # se o cliente não foi encontrado
+        error_msg = "Cliente não encontrado na base :/"
+        logger.warning(f"Erro ao buscar cliente '{cliente_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+    else:
+        logger.debug(f"Cliente econtrado: '{cliente.nome}'")
+        # retorna a representação de cliente
+        return apresenta_cliente(cliente), 200
+
+
+@app.delete('/cliente', tags=[cliente_tag],
+            responses={"200": ClienteDelSchema, "404": ErrorSchema})
+def del_cliente(query: ClienteBuscaSchema):
+    """Deleta um Cliente a partir do nome de cliente informado
+
+    Retorna uma mensagem de confirmação da remoção.
+    """
+    cliente_nome = unquote(unquote(query.nome))
+    print(cliente_nome)
+    logger.debug(f"Deletando dados sobre cliente #{cliente_nome}")
+    # cria conexão com a base
+    session = Session()
+    # faz a remoção
+    count = session.query(Cliente).filter(Cliente.nome == cliente_nome).delete()
+    session.commit()
+
+    if count:
+        # retorna a representação da mensagem de confirmação
+        logger.debug(f"Deletado cliente#{cliente_nome}")
+        return {"mesage": "Cliente removido", "id": cliente_nome}
+    else:
+        # se o cliente não foi encontrado
+        error_msg = "Cliente não encontrado na base :/"
+        logger.warning(f"Erro ao remover cliente #'{cliente_nome}', {error_msg}")
+        return {"mesage": error_msg}, 404
+
+
+@app.post('/cometario', tags=[comentario_tag],
+          responses={"200": ClienteViewSchema, "404": ErrorSchema})
+def add_comentario(form: ComentarioSchema):
+    """Adiciona de um novo comentário à um clientes cadastrado na base identificado pelo id
+
+    Retorna uma representação dos clientes e comentários associados.
+    """
+    cliente_id  = form.cliente_id
+    logger.debug(f"Adicionando comentários ao cliente #{cliente_id}")
+    # cria conexão com a base
+    session = Session()
+    # faz a busca pelo cliente
+    cliente = session.query(Cliente).filter(Cliente.id == cliente_id).first()
+
+    if not cliente:
+        # se cliente não for encontrado
+        error_msg = "Cliente não encontrado na base :/"
+        logger.warning(f"Erro ao adicionar comentário ao cliente '{cliente_id}', {error_msg}")
+        return {"mesage": error_msg}, 404
+
+    # cria o comentário
+    texto = form.texto
+    comentario = Comentario(texto)
+
+    # adiciona o comentário ao cliente
+    cliente.adiciona_comentario(comentario)
+    session.commit()
+
+    logger.debug(f"Adicionado comentário ao cliente #{cliente_id}")
+
+    # retorna a representação de cliente
+    return apresenta_cliente(cliente), 200
